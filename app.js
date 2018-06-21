cc183cc9cd149e0da58738489ca636066303bb5e
Jobayer-Ahmed <jobayeramickey@gmail.com>
2018-06-21 09:31:50 +0600

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+function magnitudeToNumber(str) {
+	const Magnitude = { 'K': 1e3, 'M': 1e6, 'B': 1e9, 'T': 1e12 };
+	if (str.search(/[a-z]/ig) !== -1) {
+		string = str.replace(/[^a-z]/ig, '').toString().toUpperCase()
+		return parseInt(parseFloat(str) * Magnitude[string]);
+	} else
+		return parseInt(parseFloat(str))
+}
+
+module.exports = magnitudeToNumber;
---
+++ readme.md
@@ -0,0 +1,9 @@
+Converts Billion, Million and Trillion strings to integer numbers.
+
+```js
+magnitudeToNumber('100,000') // 100000 
+magnitudeToNumber('1M') // 1000000
+magnitudeToNumber('2M') // 2000000
+magnitudeToNumber('2.5M') // 2500000
+magnitudeToNumber('5M') // 5000000
+```
